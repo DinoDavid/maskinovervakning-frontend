b2e2d6cb2d0f47087db816502eb93c6ea1cd3e82
kulesko <DinoDavid@users.noreply.github.com>
2025-10-16 14:39:18 +0200

    fix 9

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/maskinovervakning-frontend/', 
-  build: {
-    sourcemap: false
-  }
+  base: '/maskinovervakning-frontend/'
 })
