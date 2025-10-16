c3fff6dc2885cb58e25a23da76390ce9022352b2
kulesko <DinoDavid@users.noreply.github.com>
2025-10-16 13:33:26 +0200

    fix 6

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/maskinovervakning-frontend/',  // 👈 legg inn repo-navnet her nøyaktig som på GitHub
   build: {
     sourcemap: false
   }
